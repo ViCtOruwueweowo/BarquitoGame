2bf35c74d31ec2b4960695201e5152f3a46b603f
RickGrimmes <babajasgo@hotmail.com>
2024-04-18 12:13:07 -0600

    gg

--- src/app/Core/Components/iniciar/iniciar.component.ts
+++ src/app/Core/Components/iniciar/iniciar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WebsocketService } from '../../../web-socket.service';
 import { Router, RouterLink } from '@angular/router';
 import { GameService } from '../../Service/game.service';
 import { Game } from '../../Interface/game';
@@ -26,14 +27,21 @@ export class IniciarComponent {
 
   constructor(
     private router: Router,
-    private gameService:GameService,
+    private gameService:GameService, private websocketService: WebsocketService, // aquí se inyecta el servicio
   ) { }
+
+  ngOnInit() {
+    this.websocketService.onNewMessage().subscribe(message => {
+      console.log("mensaje recibido: ", message);
+    });
+  }
 
   public crearPartida(){
     const token = localStorage.getItem('token'); // Asegúrate de reemplazar 'token' con la clave que usaste para almacenar el token en el Local Storage
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
     this.gameService.CrearPartida(this.game, headers).subscribe(
       (response)=>{
+        this.websocketService.sendMessage('Partida creada');
         this.router.navigate(['/Game'])
       }
     )
